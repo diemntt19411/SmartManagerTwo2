7a4bea3c444ef280aae12f7318f329e366721f3d
Truc Ngan <ngannguyen.stp@gmail.com>
2021-10-28 23:13:15 +0700

    ThemMoi

---
+++ app/src/main/java/com/example/smartmanagertwo/ThemMoi.java
@@ -0,0 +1,14 @@
+package com.example.smartmanagertwo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThemMoi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.them_moi);
+    }
+}
