1eb2641643ae2cc5cbd6497ffe3cbb7fc8a6f0c9
nguyenthingan19062001 <ngannt19411@st.uel.edu.vn>
2021-10-29 16:50:13 +0700

    TaiKhoanActivity

--- app/src/main/java/com/example/smartmanagertwo/TaiKhoan.java
+++ app/src/main/java/com/example/smartmanagertwo/TaiKhoan.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ThongKe extends AppCompatActivity {
+public class TaiKhoan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sua_thong_tin);
+        setContentView(R.layout.activity_tai_khoan_chi_tiet);
     }
 }
