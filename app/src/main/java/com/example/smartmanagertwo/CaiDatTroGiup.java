01d1a05c53f8c5b30331d90ff6b9cc5664a5ebd4
Truc Ngan <ngannguyen.stp@gmail.com>
2021-10-30 23:39:39 +0700

    ThemMoi

---
+++ app/src/main/java/com/example/smartmanagertwo/CaiDatTroGiup.java
@@ -0,0 +1,14 @@
+package com.example.smartmanagertwo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CaiDatTroGiup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cai_dat_sao_luu_gg_drive);
+    }
+}
