74dcd9618c9138f5d828ba9bc1dea15893d459b4
thamnth <thamnth19411@st.uel.edu.vn>
2021-10-31 09:14:33 +0700

    Update

---
+++ app/src/main/java/com/example/smartmanagertwo/TinTucFragment1.java
@@ -0,0 +1,17 @@
+package com.example.smartmanagertwo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TinTucFragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tintuc1, container, false);
+    }
+}
